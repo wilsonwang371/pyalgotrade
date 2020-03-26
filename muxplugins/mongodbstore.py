49f56b8d0c1aee3b8715a7736c2e254a82ba89c8
Wilson Wang <wilsonwang371@users.noreply.github.com>
2020-03-26 12:41:21 -0700

    add inial file for MongoDB store

---
+++ muxplugins/mongodbstore.py
@@ -0,0 +1,18 @@
+import six
+
+import coloredlogs
+import pyalgotrade.logger
+from pyalgotrade.apps.utils.muxplugin import MuxPlugin
+
+coloredlogs.install(level='INFO')
+logger = pyalgotrade.logger.getLogger(__name__)
+
+
+class MongoDBMuxPlugin(MuxPlugin):
+
+    def __init__(self):
+        pass
+
+    def process(self, key, data):
+        #TODO: save data to mongodb
+        pass
