e7938242d7dc75c94124e9c1cf6303b830847bc8
Wilson Wang <wilsonny371@gmail.com>
2020-03-29 15:22:14 -0700

    bug fix

--- pyalgotrade/optimizer/xmlrpcserver.py
+++ pyalgotrade/optimizer/xmlrpcserver.py
@@ -157,7 +157,7 @@ class Server(xmlrpc_server.SimpleXMLRPCServer):
             # Initialize instruments, bars and parameters.
             logger.info("Loading bars")
             loadedBars = []
-            for dateTime, bars in self.__barFeed:
+            for dateTime, bars, freq in self.__barFeed:
                 loadedBars.append(bars)
             instruments = self.__barFeed.getRegisteredInstruments()
             self.__instrumentsAndBars = serialization.dumps((instruments, loadedBars))
